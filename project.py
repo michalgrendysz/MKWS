bd2bfe1ff398596879df219d0124ef5ef4c386aa
michalgrendysz <39854656+michalgrendysz@users.noreply.github.com>
2018-06-14 19:07:21 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,46 +1,46 @@
 import cantera as ct
 import csv
 
-cti_file='nasa.cti'
+cti_file='~/Dokumenty/nasa.cti'
 
-m=0.002 #kg
-area=0.05 #m^2
-mpiston=2.0 #kg
-tstep=0.001 #s
-lengthstart=0.2 #m
-lengthstop=0.4 #m
+mgp=0.002 #[kg] mass of gunpowder
+area=0.05 #[m^2] frontal area of a piston
+mpiston=2.0 #[kg] mass of the piston
+tstep=0.001 #[s] time step of a simulation
+lengthstart=0.2 #[m] initial length of a reactor
+lengthstop=0.4 #[m] final length of a reactor
 
-air = ct.Solution(cti_file,'air')
+air = ct.Solution(cti_file,'gas')
 air.TP= 300.0, 1.0*ct.one_atm
 
-KNO3 = ct.Solution(cti_file,'KNO3')
+KNO3 = ct.Solution(cti_file,'ox')
 KNO3.TP= 300.0, 1.0*ct.one_atm
-q1=ct.Quantity(KNO3, mass=0.75*m)
+q1=ct.Quantity(KNO3, mass=0.75*mgp)
 
-S = ct.Solution(cti_file,'S')
+S = ct.Solution(cti_file,'sulfur')
 S.TP=300.0, 1.0*ct.one_atm
-q2=ct.Quantity(S, mass=0.1*m)
+q2=ct.Quantity(S, mass=0.1*mgp)
 
-C = ct.Solution(cti_file,'C')
+C = ct.Solution(cti_file,'carbon')
 C.TP=300.0, 1.0*ct.one_atm
-q3=ct.Quantity(C, mass=0.15*m)
+q3=ct.Quantity(C, mass=0.15*mgp)
 
 products = ct.Solution(cti_file,'products')
 products.TP=300.0, 1.0*ct.one_atm
 q4=ct.Quantity(products, mass=0)
 
-K2S=ct.Solution(cti_file,'K2S')
+K2S=ct.Solution(cti_file,'potsulf')
 K2S.TP=300.0, 1.0*ct.one_atm
 q5=ct.Quantity(K2S, mass=0)
 
 inter = ct.Interface(cti_file,'surface',[q1,q2,q3,q4,q5])
-inter.TP=300.0, 1.0*ct.one_atm
+inter.TP=500.0, 1.0*ct.one_atm
 
 env=ct.Reservoir(air)
 
 reactor=ct.Reactor(q1+q2+q3+q4+q5)
 reactor.volume = lengthstart*area
-ct.ReactorSurface(inter,reactor,A=100.0)
+ct.ReactorSurface(surface,reactor,A=(mgp/(1700))*(6/10^-3))
 
 w=ct.Wall(reactor,env,A=area,K=tstep*area/mpiston)
 
@@ -56,8 +56,6 @@ csvfile.writerow("time [s]","temperature [K]", "pressure [Pa]", "volume [m^3]", 
 print "Current simulation time:"
 
 while time<120.0 and reactor.volume<area*lengthstop:
-	if time==1.0:
-		inter.TP=500.0,1.0*ct.one_atm
 	csvfile.writerow(time,reactor.thermo.T,reactor.thermo.P,reactor.volume,w.velocity)
 	print time+" s"
 	time+=tstep
